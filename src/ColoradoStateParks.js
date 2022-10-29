5b7be3330b32b0aeff854cb1a4d6a325b537d4d7
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-10-29 13:09:09 -0500

    commit tree 1e073e933bf6eb7cf8ed325e365665af623b1d9c

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,34 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain"
+// import { trees, wildlife } from "./parks/RockyMountain"
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildLife,
+// } from "./parks/RockyMountain"
 
-function ColoradoStateParks() {
+// console.log(parkTrees)
+// parkWildLife();
+
+// console.log(trees)
+// wildlife()
+
+// console.log(trees);
+// wildlife();
+
+// console.log(RMFunctions.trees)
+
+// RMFunctions.wildlife();
+
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  <div>
+  <MesaVerde />
+  </div>
   return <h1>Colorado State Parks!</h1>;
+
 }
+
+// export default ColoradoStateParks;
